5c3a24d0fb800068c48f1d83714daa61fbd21386
SumaDixit1793 <dsuma17@gmail.com>
2022-04-12 20:43:47 +0530

    Admission Checker API

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { UsersService } from '../users.service'
+import { Education } from './education'
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
@@ -16,11 +18,18 @@ export class EducationComponent implements OnInit {
     4. degree (dropdown)
     5. major (dropdown)
     */
-    start_year:Date;
-    end_year:Date;
-    graduated:boolean;
-    degree:string;
-    major:string;
+
+    
+    start_year_1:Date;
+    end_year_1:Date;
+    graduated_1:boolean;
+    degree_1:string;
+    major_1:string;
+    start_year_2:Date;
+    end_year_2:Date;
+    graduated_2:boolean;
+    degree_2:string;
+    major_2:string;
     hidden_education_div:boolean;
     showFirst:boolean;
     available_degrees = [
@@ -50,34 +59,118 @@ export class EducationComponent implements OnInit {
       },
     ];
 
-
+    index:number;    
     constructor(private user:UsersService) {
-      this.start_year=new Date();
-      this.end_year=new Date();
-      this.graduated=true;
-      this.degree='B.E';
-      this.major='Computer Science';
+      this.start_year_1=new Date("2022-03-25");
+      this.end_year_1=new Date("2026-12-31");
+      this.graduated_1=true;
+      this.degree_1='B.E';
+      this.major_1='Computer Science';
+      this.start_year_2=new Date("2022-03-25");
+      this.end_year_2=new Date("2026-12-31");
+      this.graduated_2=true;
+      this.degree_2='B.E';
+      this.major_2='Computer Science';
       this.hidden_education_div=true;
       this.showFirst=true;
+      this.index = 1;
       console.log(this);
      }
 
-     set_graduated(){
-      this.graduated = !(this.graduated);
+     set_start_year_component_1(start_year:any)
+     {
+        this.start_year_1 = start_year;
      }
-     get_graduated(){
-       return this.graduated;
+
+     set_end_year_component_1(end_year:any)
+     {
+        this.end_year_1 = end_year;
      }
+
+     set_graduated_component_1(graduated:any)
+     {
+        this.graduated_1 = !(this.graduated_1);
+     }
+
+     set_degree_component_1(event:Event)
+     {
+        this.degree_1 = (event.target as HTMLSelectElement).value;;
+     }
+
+     set_major_component_1(event:Event)
+     {
+        this.major_1 = (event.target as HTMLSelectElement).value;;
+     }
+
+     //For second education component
+     set_start_year_component_2(start_year:any)
+     {
+        this.start_year_2 = start_year;
+     }
+
+     set_end_year_component_2(end_year:any)
+     {
+        this.end_year_2 = end_year;
+     }
+
+     set_graduated_component_2(graduated:any)
+     {
+        this.graduated_2 = !(this.graduated_2);
+     }
+
+     set_degree_component_2(event:Event)
+     {
+        this.degree_2 = (event.target as HTMLSelectElement).value;;
+     }
+
+     set_major_component_2(event:Event)
+     {
+        this.major_2 = (event.target as HTMLSelectElement).value;;
+     }
+
      add_education()
      {
-      this.showFirst = !(this.showFirst);
-      this.hidden_education_div = !(this.hidden_education_div); 
+        this.index == 1 ? this.index += 1 : this.index -= 1;
+        this.showFirst = !(this.showFirst);
+        this.hidden_education_div = !(this.hidden_education_div); 
+        if(this.index == 1)
+        {
+          //We removed a hidden_education_div, hence 2nd education object needs cleared
+          this.start_year_2=new Date("2022-03-25");
+          this.end_year_2=new Date("2026-12-31");
+          this.graduated_2=true;
+          this.degree_2='B.E';
+          this.major_2='Computer Science';
+        }
      }
-    ngOnInit(): void {
+    ngOnInit(): void 
+    {
+
     }
     check_admission()
     {
-      this.user.setParams(this.graduated ? "1" : "0")
+      //Send the params to the API in the form of an array of Objects,
+      //If index value == 2 then 2 values in the education array, else 1
+      //Update the education object everytime based on the index
+
+      const obj_1 = {
+        "start_year" : this.start_year_1,
+        "end_year" : this.end_year_1,
+        "graduated" : this.graduated_1,
+        "degree" : this.degree_1,
+        "major" : this.major_1,
+      }
+
+      const obj_2 = {
+        "start_year" : this.start_year_2,
+        "end_year" : this.end_year_2,
+        "graduated" : this.graduated_2,
+        "degree" : this.degree_2,
+        "major" : this.major_2,
+      }
+      let obj_1_str = JSON.stringify(obj_1);
+      let obj_2_str = JSON.stringify(obj_2);
+      this.user.setParamsEducation([obj_1,obj_2]);
       this.user.getData().subscribe(data=>{
         console.warn(data);
         var x =JSON.parse(JSON.stringify(data));
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { HttpParams } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
@@ -7,21 +8,31 @@ import { HttpClient } from '@angular/common/http'
 export class UsersService {
 
   url:string;
-  
+  education_array:Object[];
   constructor(private http:HttpClient) 
   { 
 
-  	this.url = "";
+  	this.url = "http://127.0.0.1:5000/post_education";
+  	this.education_array = new Array<Object>(2);
   }
-  setParams(graduated:string)
+
+  setParamsEducation(education_array:Object[])
   {
-  	this.url = "http://127.0.0.1:5000/post_education?graduated=" + graduated;
+  	for(var i = 0;i<education_array.length;i++)
+  	{
+  		this.education_array[i] = education_array[i];
+  	}
   }
   getData()
   {
   	
     // Fake API testing : https://jsonplaceholder.typicode.com/
     const headers = { 'content-type': 'application/json'} 
-    return this.http.get(this.url, {'headers':headers});
+    let queryParams = new HttpParams();
+    for(var i = 0;i<this.education_array.length;i++)
+    {
+    	queryParams = queryParams.append("obj_" +(i+1) , JSON.stringify(this.education_array[i]));
+    }
+    return this.http.get(this.url, {'headers':headers,params:queryParams});
   }
 }
